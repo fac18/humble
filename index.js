397cdffe50b6871bbd1d7e8d4ea756250752fb85
Judith <jyouziel20@hotmail.com>
2020-01-21 16:35:09 +0000

    Set up express server domain

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/public/index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port);
+
+console.log(`Listening on port ${port}`);
+
+
