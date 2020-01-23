4e85e906a4e421a2bb495619130223afd9cc5542
kin-au <kinnamau@gmail.com>
2020-01-23 12:03:29 +0000

    create router.js and fix routes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
+const router = require("./router");
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(router);
 
 const port = process.env.PORT || 3001;
-app.listen(port);
-
-console.log(`Listening on port ${port}`);
-
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
