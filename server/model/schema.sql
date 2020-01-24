84ca0c996f96da2c85f58facad299e0396c229b2
ReginaldJbeili <reginaldjbeili@gmail.com>
2020-01-24 15:56:16 +0000

    add offers and requests dummy data

--- server/model/schema.sql
+++ server/model/schema.sql
@@ -99,14 +99,15 @@ INSERT INTO categories (category_name) VALUES
 ('cooking')
 ;
 INSERT INTO offers (category_id, offer_name, offer_description, member_id) VALUES
-(1, 'painting' , 'I would like to share my single-stroke prowess with a canvass', 1),
-('judith@fac.com', 'passowrd2', 'Judith', 'N153XF', 'https://image.shutterstock.com/image-photo/big-bull-young-strong-have-260nw-754733863.jpg'),
-('dan@fac.com', 'password3', 'Dan', 'N154ZS', 'https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg'),
-('kin@fac.com', 'password4', 'Kin', 'N159BT', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/175.png');
-INSERT INTO members (email, hashed_password, member_name, postcode, avatar_url) VALUES
-('regi@fac.com', 'password1' , 'Reggie', 'N154UY', 'https://previews.123rf.com/images/cteconsulting/cteconsulting1405/cteconsulting140500148/28827832-an-image-of-a-teddy-bear-face.jpg'),
-('judith@fac.com', 'passowrd2', 'Judith', 'N153XF', 'https://image.shutterstock.com/image-photo/big-bull-young-strong-have-260nw-754733863.jpg'),
-('dan@fac.com', 'password3', 'Dan', 'N154ZS', 'https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg'),
-('kin@fac.com', 'password4', 'Kin', 'N159BT', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/175.png');
+(1, 'painting' , 'I would like to share my single-stroke prowess with a canvass', 4),
+(2, 'woodwork' , 'My cabinet-making skills have changed the world and I want to share it with someone', 3),
+(3, 'football' , 'Teaching a bicycle kick to a neighbour would be great', 2),
+(4, 'hotpot' , 'Showing a community member how to setup a hotpot brings a great feeling to me', 1)
+;
+INSERT INTO requests (category_id, request_name, request_description, member_id) VALUES
+(1, 'drawing' , 'Needs help learning to draw on a graphic tablet', 1),
+(2, 'remodelling' , 'My second baby has a crib that needs remodelling but I would love if someone could teach me how to do it', 2),
+(3, 'swimming' , 'Would love it if anyone could help me with backstroke', 3),
+(4, 'sushi' , 'Will appreciate learning how to make salmon-skin sushi from a potential friend', 4);
 
 COMMIT;
