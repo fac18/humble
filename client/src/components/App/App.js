f26b94e95b53e02629bd9882250f9c309d883652
Judith <jyouziel20@hotmail.com>
2020-01-27 17:30:51 +0000

    Add Switch element and route tags to app component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -5,20 +5,28 @@ import "./App.css";
 import getRequest from "../../utils/getRequest";
 
 function App() {
-  const [dan, setDan] = useState(null);
-
-  useEffect(() => {
-    getRequest("/get-member?member_id=3").then(res => {
-      setDan(res.member_name);
-    });
-  }, []);
-
   return (
-    <div className="app">
-      <p>This is our app.</p>
-      {dan ? <p>One of the members is called {dan}.</p> : null}
-    </div>
+    <main>
+      <Switch>
+        <Route path="/" component={Landing} />
+        <Route path="/about" component={About} />
+        <Route path="/search" component={Search} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/dan" component={Dan} />
+      </Switch>
+    </main>
   );
 }
 
 export default App;
+
+/*const [dan, setDan] = useState(null); useEffect(() =>{" "}
+        {getRequest("/get-member?member_id=3").then(res => {
+          setDan(res.member_name);
+        })}
+        , []); return (
+        <div className="app">
+          <p>This is our app.</p>
+          {dan ? <p>One of the members is called {dan}.</p> : null}
+        </div>
+        );*/
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
