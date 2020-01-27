d568f13917230c9a4d6b7167c85da4ac722d8a93
Judith <jyouziel20@hotmail.com>
2020-01-27 18:08:57 +0000

    Add components files

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
+//import components
+import Landing from "../Landing";
+import About from "../About";
+import Search from "../Search";
+import Profile from "../Profile";
+import Dan from "../Dan";
+import Error from "../Error";
+
 import getRequest from "../../utils/getRequest";
 
 function App() {
@@ -13,6 +21,7 @@ function App() {
         <Route path="/search" component={Search} />
         <Route path="/profile" component={Profile} />
         <Route path="/dan" component={Dan} />
+        <Route component={Error} />
       </Switch>
     </main>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div>
+      <Link to="/">Landing</Link>
+      <Link to="/about">About us</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/dan">Dan</Link>
+    </div>
+  );
+}
