1e13ce130b2369d360d442d8a02be3c0acb32d40
kin-au <kinnamau@gmail.com>
2020-01-28 12:44:13 +0000

    add member email to getData db query

--- server/queries/getData.js
+++ server/queries/getData.js
@@ -3,7 +3,7 @@ const dbConnection = require("../model/dbConnection");
 const getMember = id => {
   return dbConnection
     .query(
-      "SELECT member_name, postcode, avatar_url FROM members WHERE member_id=$1",
+      "SELECT member_name, email, postcode, avatar_url FROM members WHERE member_id=$1",
       [id]
     )
     .then(data => data.rows[0]);
