5fe33bca8a68aef7219288714907411308402ab9
Judith <jyouziel20@hotmail.com>
2020-01-28 16:25:19 +0000

    Add About us content

---
+++ client/src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="app">
+      <p>
+        Welcome to humble. Our goal is to promote community solidarity. We have
+        built a platform to help facilitate real-life human interactions that
+        are non-transactional in nature.
+      </p>
+      <p>
+        We want to help communities share their life skills, experience and
+        knowledge amongst themselves for free, without the exchange of money, on
+        a non-commercial basis.
+      </p>
+      <p>
+        If you would like to join our platform, please read and confirm your
+        agreemennt with our statement of intent.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
