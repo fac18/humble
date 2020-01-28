e0dd82bba5ab0f3dec22fd245fe5a9eb3a9a5188
ReginaldJbeili <53199009+ReginaldJbeili@users.noreply.github.com>
2020-01-28 12:48:04 +0000

    Update App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -16,6 +16,15 @@ import getRequest from "../../utils/getRequest";
 //import getRequest from "../../utils/getRequest";
 
 function App() {
+  
+  // const [dan, setDan] = useState(null);
+ //
+ // useEffect(() => {
+   // getRequest("/get-member?member_id=3").then(res => {
+     // setDan(res.member_name);
+   // });
+ // }, []);
+  
   return (
     <main>
       <Switch>
