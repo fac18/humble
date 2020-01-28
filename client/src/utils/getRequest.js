ee4007cc062edc888d88de358d003e69362b4204
groupanimal <groupanimal@posteo.net>
2020-01-28 21:53:37 +0000

    return dev:build scripts to original condition and solve domain assignment in getRequest by considering window.location object

--- client/src/utils/getRequest.js
+++ client/src/utils/getRequest.js
@@ -1,21 +1,14 @@
-// consider environment in order to assign domain appropriately
+// consider window.location object in order to assign domain appropriately
+const hostname = window && window.location && window.location.hostname;
 const domain =
-  process.env.NODE_ENV === "production"
-    ? "https://wearehumble.herokuapp.com"
-    : "http://localhost:3001";
+  hostname === "localhost"
+    ? "http://localhost:3001"
+    : "https://wearehumble.herokuapp.com";
 
 const getRequest = endpoint => {
-  console.log(domain); // is printing heroku domain despite not declaring production environment in scripts
-  // return fetch(domain + endpoint)
-  return fetch("http://localhost:3001" + endpoint) // hard code localhost domain for now
+  return fetch(domain + endpoint)
     .then(res => res.json())
     .catch(console.log);
 };
-
-// const getRequest = endpoint => {
-//   return fetch("http://localhost:3001" + endpoint)
-//     .then(res => res.json())
-//     .catch(console.log);
-// };
 
 export default getRequest;
