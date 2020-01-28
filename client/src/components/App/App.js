ec19f78691907a18062406313b8dd3fce1ff8557
Judith <jyouziel20@hotmail.com>
2020-01-28 12:17:10 +0000

    Add dummy content to landing page

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 //import components
@@ -7,20 +7,18 @@ import Landing from "../Landing";
 import About from "../About";
 import Search from "../Search";
 import Profile from "../Profile";
-import Dan from "../Dan";
 import Error from "../Error";
 
-import getRequest from "../../utils/getRequest";
+//import getRequest from "../../utils/getRequest";
 
 function App() {
   return (
     <main>
       <Switch>
-        <Route path="/" component={Landing} />
+        <Route path="/" component={Landing} exact />
         <Route path="/about" component={About} />
         <Route path="/search" component={Search} />
         <Route path="/profile" component={Profile} />
-        <Route path="/dan" component={Dan} />
         <Route component={Error} />
       </Switch>
     </main>
@@ -28,14 +26,3 @@ function App() {
 }
 
 export default App;
-
-/*const [dan, setDan] = useState(null); useEffect(() =>{" "}
-        {getRequest("/get-member?member_id=3").then(res => {
-          setDan(res.member_name);
-        })}
-        , []); return (
-        <div className="app">
-          <p>This is our app.</p>
-          {dan ? <p>One of the members is called {dan}.</p> : null}
-        </div>
-        );*/
--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,1 +1,1 @@
-<h1>Oops! Page not found!</h1>;
+//<h1>Oops! Page not found!</h1>;
