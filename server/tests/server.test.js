826a8f964f6020616f64133e7ee1cefa0dbd5480
kin-au <kinnamau@gmail.com>
2020-01-29 15:29:15 +0000

    rename server route test for search-offer-category

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -31,7 +31,7 @@ test("test search-offer-all endpoint path works", () => {
   });
 });
 
-test("test search-offer endpoint path works", () => {
+test("test search-offer-category endpoint path works", () => {
   return request.get("/search-offer-category?category_id=3").then(response => {
     expect(response.statusCode).toBe(200);
   });
