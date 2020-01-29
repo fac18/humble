fd1a6925c5f889bba7657ebc44af031822b3b0d4
Judith <jyouziel20@hotmail.com>
2020-01-29 15:41:56 +0000

    Load maps javascript API using a script tag

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,8 +1,16 @@
 import React, { Component, createRef } from "react";
 import "./Map.css";
 
+const GOOGLE_MAP_API_KEY = AIzaSyASa9m5akT8pSnC7lhiUUPN8ajPneNEI_U;
+
 export class GoogleMap extends Component {
   googleMapRef = createRef();
+
+  componentDidMount() {
+    const googleMapScript = document.createElement("script");
+
+    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
+  }
 
   createGoogleMap = () =>
     new window.google.maps.Map(this.googleMapRef.current, {
