e6af5932350d800cb59de23da9027fd14a950655
Judith <jyouziel20@hotmail.com>
2020-01-29 15:25:39 +0000

    Create map div element

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,4 +1,16 @@
 import React, { Component, createRef } from "react";
 import "./Map.css";
 
-export class GoogleMap extends Component {}
+export class GoogleMap extends Component {
+  googleMapRef = createRef();
+
+  render() {
+    return (
+      <div
+        id="google-map"
+        ref={this.googleMapRef}
+        style={{ width: "400px", height: "300px" }}
+      />
+    );
+  }
+}
