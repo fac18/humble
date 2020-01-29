6a7bc2d48cef70c6a41e8b8169f90c27adc39f74
Judith <jyouziel20@hotmail.com>
2020-01-29 15:32:32 +0000

    Add function that creates a map of Finsbury Park area in the div

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -4,6 +4,16 @@ import "./Map.css";
 export class GoogleMap extends Component {
   googleMapRef = createRef();
 
+  createGoogleMap = () =>
+    new window.google.maps.Map(this.googleMapRef.current, {
+      zoom: 15,
+      center: {
+        lat: 51.5712,
+        lng: -0.1009
+      },
+      disableDefaultUI: true
+    });
+
   render() {
     return (
       <div
