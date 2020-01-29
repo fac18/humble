e168c119fdbf6f5cc4cfda161468acf5d95de7db
Judith <jyouziel20@hotmail.com>
2020-01-29 13:51:13 +0000

    Ammend About copy

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,18 +5,19 @@ const AboutPage = () => {
   return (
     <div className="app">
       <p>
-        Welcome to humble. Our goal is to promote community solidarity. We have
-        built a platform to help facilitate real-life human interactions that
+        Welcome to humble. Our goal is to promote community solidarity. We hope
+        to help communities become kinder, more supportive and joy-filled. This
+        platform has been built to facilitate real-life human interactions that
         are non-transactional in nature.
       </p>
       <p>
-        We want to help communities share their life skills, experience and
-        knowledge amongst themselves for free, without the exchange of money, on
-        a non-commercial basis.
+        We aim to help communities share their life skills, experience and
+        knowledge amongst themselves free of charge, without the exchange of
+        money, on a non-commercial basis.
       </p>
       <p>
         If you would like to join our platform, please read and confirm your
-        agreemennt with our statement of intent.
+        agreement with our statement of intent.
       </p>
     </div>
   );
