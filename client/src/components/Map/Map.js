43ae03a9387d1057ebe7368249298ea99f20ea28
Judith <jyouziel20@hotmail.com>
2020-01-30 11:34:16 +0000

    Rewrite component as a functional component, using React Hooks

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,47 +1,55 @@
-/*import React, { Component, createRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./Map.css";
 
 const GOOGLE_MAP_API_KEY = AIzaSyASa9m5akT8pSnC7lhiUUPN8ajPneNEI_U;
+const myLocation = {
+  // Founders and Coders
+  lat: 51.5712,
+  lng: -0.1009
+};
 
-export class GoogleMap extends Component {
-  googleMapRef = React.createRef();
+const mapStyles = {
+  width: "100%",
+  geight: "400px"
+};
 
-  componentDidMount() {
-    const googleMapScript = document.createElement("script");
+function GoogleMaps(props) {
+  const geegleMapRef = React.createRef();
+  const googleMap = useRef(null);
+  const marker = useRef(null);
 
-    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
-
-    window.document.body.appendChild(googleScript)
-
-    googleScript.addEventListener('load', {
-        this.googleMap = this.createGoogleMap(),
-        this.marker = this.createMarker()
-    })
-  }
-
-  createGoogleMap = () =>
-    new window.google.maps.Map(this.googleMapRef.current, {
+  // helper functions
+  const createGoogleMap = () =>
+    new window.google.maps.Map(googleMapRef.current, {
       zoom: 15,
       center: {
+        // Finsbury Park
         lat: 51.5712,
         lng: -0.1009
-      },
-      disableDefaultUI: true
+      }
     });
 
-    createMarker = () => new window.google.maps.Marker({
-        position: { lat: 51.5637, lng: -0.1077 },
-        map: this.googleMap,
-        title: 'founders and coders' 
-    })
+  const createMarker = () =>
+    new window.google.maps.Marker({
+      position: {
+        lat: myLocation.lat,
+        lng: myLocation.lng
+      }
+    });
 
-  render() {
-    return (
-      <div
-        id="google-map"
-        ref={this.googleMapRef}
-        style={{ width: "400px", height: "300px" }}
-      />
-    );
-  }
-}*/
+  // useEffect Hook
+  useEffect(() => {
+    const googleMapScript = document.createElement("script");
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
+    window.document.body.appendChild(googleMapScript);
+
+    googleMapScript.addEventListener("load", () => {
+      googleMap.current = createGoogleMap();
+      marker.current = createMarker();
+    });
+  });
+
+  return <div id="google-map" ref={googleMapRef} style={mapStyles} />;
+}
+
+export default GoogleMaps;
