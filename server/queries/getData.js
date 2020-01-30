febf060276b763f247889d8641dbe7e9c1c75a13
kin-au <kinnamau@gmail.com>
2020-01-30 12:04:54 +0000

    add db query for searchRequestAll

--- server/queries/getData.js
+++ server/queries/getData.js
@@ -50,11 +50,20 @@ const searchOfferCategory = category => {
     .then(data => data.rows);
 };
 
+const searchRequestAll = () => {
+  return dbConnection
+    .query(
+      "SELECT members.member_id, members.member_name, members.postcode, members.avatar_url, categories.category_name, requests.request_name, requests.request_description FROM requests JOIN members ON members.member_id=requests.member_id JOIN categories ON categories.category_id=requests.category_id"
+    )
+    .then(data => data.rows);
+};
+
 module.exports = {
   getMember,
   getOffer,
   getRequest,
   categoryList,
   searchOfferAll,
-  searchOfferCategory
+  searchOfferCategory,
+  searchRequestAll
 };
