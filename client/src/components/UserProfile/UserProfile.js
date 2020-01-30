eca88bd137623437ee28cfdc805315f8a606224b
kin-au <kinnamau@gmail.com>
2020-01-30 19:52:20 +0000

    add user profile to return

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -17,7 +17,6 @@ const UserProfile = ({ user, userView, setUserView }) => {
   const [userRequests, setUserRequests] = useState(null);
 
   useEffect(() => {
-    console.log("getting profile");
     getRequest(`/get-profile?member_id=${user}`).then(res => {
       setUserName(res[0].member_name);
       setUserAvatar(res[0].avatar_url);
@@ -32,8 +31,35 @@ const UserProfile = ({ user, userView, setUserView }) => {
     <>
       <H2>User Profile</H2>
       {/* <Button onClick={() => setUserView(null)}>Back to Search</Button> */}
-      <H3>userName is: {userName}</H3>
-      <P>Test p component</P>
+      <H3>Viewing {userName}'s Profile</H3>
+
+      {userAvatar ? <img src={userAvatar} /> : null}
+      {userEmail ? <p>Contact email: {userEmail}.</p> : null}
+      {userPostcode ? <p>Location: {userPostcode}</p> : null}
+      <p>Offers:</p>
+      {userOffers ? (
+        <ul>
+          {userOffers.map(offer => (
+            <li key={offer.offer_name}>
+              {offer.offer_name}: {offer.offer_description}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>{userName} does not currently have any offers</p>
+      )}
+      <p>Requests:</p>
+      {userRequests ? (
+        <ul>
+          {userRequests.map(request => (
+            <li key={request.request_name}>
+              {request.request_name}: {request.request_description}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>{userName} does not currently have any requests</p>
+      )}
 
       <Navbar />
     </>
