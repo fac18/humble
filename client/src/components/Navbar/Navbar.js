c3840e2505cece3cd38d5fa8795128638c73026f
kin-au <kinnamau@gmail.com>
2020-01-31 11:15:19 +0000

    fix imports and tests

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Navbar.css";
 import { Link } from "react-router-dom";
 import NavContainer from "../styled/NavContainer";
 import NavIcon from "../styled/NavIcon";
