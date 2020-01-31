95ff92c8b2deedee7e8e48a9400f8e4e693a637b
ReginaldJbeili <reginaldjbeili@gmail.com>
2020-01-31 12:46:23 +0000

    add test for app

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
 
-// commented out test for now since we don't know how to test React Router
-
-test("renders learn react link", () => {
-  // const { getByText } = render(<App />);
-  // getByText('This is our app.')
+test("component works as expected", () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  getByText("What is humble?");
 });
